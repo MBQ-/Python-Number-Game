ccee20e74e4db3027301baaa945c22006f6405a3
Kyler <mbqsniper@gmail.com>
2013-09-11 18:32:43 -0700

    lil more

--- NumberGuessingGame.py
+++ NumberGuessingGame.py
@@ -10,3 +10,10 @@ print('Hello! What is your name?')
 
 # myName will be the variable based on the input of the users name
 myName = input()
+
+# the number is the initial variable, random.randint is the random integer alongside the random numbers, 1-20
+number = random.randint(1, 20)
+# Now let's print some more text so the user knows what's going on
+print('Well, ' + myName + ', Im thinking of a number between one and twenty.')
+
+# Next

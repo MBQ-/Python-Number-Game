174d15b592dc5f0a9228f039e6f71727653efc7c
Kyler <mbqsniper@gmail.com>
2013-09-11 18:28:06 -0700

    Create NumberGuessingGame.py

---
+++ NumberGuessingGame.py
@@ -0,0 +1,12 @@
+# Loosely based on this tutorial: http://inventwithpython.com/chapter4.html
+
+import random
+
+# Variable for how many guesses have been taken. Dynamically changes
+guessesTaken = 0
+
+# Printing text that will initially pop up to essentially greet you
+print('Hello! What is your name?')
+
+# myName will be the variable based on the input of the users name
+myName = input()
